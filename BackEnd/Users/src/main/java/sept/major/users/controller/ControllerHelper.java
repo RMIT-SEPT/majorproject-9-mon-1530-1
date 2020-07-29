69267e0cffb64af9e4f6b833bee199da13b3ada2
Brodey Yendall <brodey_yendall@hotmail.com>
2020-07-29 23:21:17 +1000

    More files missed, hopefully intelliJ bug causing this is fixed

--- BackEnd/Users/src/main/java/sept/major/users/controller/ControllerHelper.java
+++ BackEnd/Users/src/main/java/sept/major/users/controller/ControllerHelper.java
@@ -1,14 +1,23 @@
 package sept.major.users.controller;
 
 import sept.major.users.exception.InvalidEntityException;
+import sept.major.users.exception.ResponseErrorFoundException;
+import sept.major.users.response.error.FieldIncorrectTypeError;
+import sept.major.users.response.error.FieldMissingError;
+import sept.major.users.response.error.ResponseError;
 
 import java.lang.reflect.Method;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public abstract class ControllerHelper<T> extends ControllerHelperBase {
 
-    public T analysePostInput(Class<T> entityClass, Map<String, Object> responseBody) {
+    public T analysePostInput(Class<T> entityClass, Map<String, Object> responseBody) throws ResponseErrorFoundException {
+        Set<ResponseError> responseErrors = new HashSet<>();
         T entity;
+
+
         try {
             entity = entityClass.newInstance();
         } catch (InstantiationException e) {
@@ -23,11 +32,23 @@ public abstract class ControllerHelper<T> extends ControllerHelperBase {
             Object responseBodyValue = responseBody.get(entry.getKey());
             if (responseBodyValue != null) {
                 try {
-                    entry.getValue().invoke(entity, responseBodyValue);
+                    Class expectedType = entry.getValue().getParameterTypes()[0];
+                    if (!responseBodyValue.getClass().equals(expectedType)) {
+                        responseErrors.add(new FieldIncorrectTypeError(entry.getKey(), expectedType.toString(), responseBodyValue.getClass().toString()));
+                    } else {
+                        entry.getValue().invoke(entity, responseBodyValue);
+                    }
+
                 } catch (Exception e) {
                     throw new RuntimeException("Received method that is invokable. Only invokable methods should be here");
                 }
+            } else {
+                responseErrors.add(new FieldMissingError(entry.getKey()));
             }
+        }
+
+        if (!responseErrors.isEmpty()) {
+            throw new ResponseErrorFoundException(responseErrors);
         }
 
         return entity;
