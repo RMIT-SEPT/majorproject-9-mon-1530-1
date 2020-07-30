b8f33a460a92b6665d33b194a08ecea895308921
Liza Tawaf <lizatawaf@gmail.com>
2020-07-31 00:41:02 +1000

    Login page is done

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
+import About from './About/About.js';
 import Form from './Form/Form.js';
 import Main from './Main/Main.js';
 import Login from './Login/Login.js';
+import TextLoop from "react-text-loop";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import {createGlobalStyle} from 'styled-components'
@@ -20,6 +22,7 @@ function App() {
       <Route path= "/main" component={Main}/>
         <Route path= "/form" component={Form}/>
         <Route path= "/login" component={Login}/>
+        <Route path= "/about" component={About}/>
       </div>
     </Router>
   );
