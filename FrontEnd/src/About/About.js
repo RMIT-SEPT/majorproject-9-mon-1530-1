ba4c269d5267f53d9b4bcb0a4798dc5b32675567
Liza Tawaf <lizatawaf@gmail.com>
2020-08-03 23:40:42 +1000

    Removed css for about page

--- FrontEnd/src/About/About.js
+++ FrontEnd/src/About/About.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import './About.css';
 import styled from 'styled-components';
 import { Button, Grid } from '@material-ui/core';
-import logo from '../media/logo.png';
-import login from '../media/login.png';
 import doctor from '../media/doc.png';
 import gym from '../media/gym.svg';
-import small from '../media/small.png';
 import TextLoop from "react-text-loop";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Toolbar from '../Toolbar/Toolbar.js';
+
+const Doc = styled.img`
+  width: 60%;
+  height: auto;
+  background-repeat: no-repeat;
+  background-size: contain;
+  padding-left: 5%;
+`
+const Gym = styled.img`
+  width: 80%;
+  height: auto;
+  background-repeat: no-repeat;
+  background-size: contain;
+`
+const Bold = styled.div`
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 35px;
+  line-height: 95px;
+  letter-spacing: -0.05em;
+  color: #FFFFFF;
+  margin: 5%;
+  white-space: 'pre-wrap';
+`
 
 const GreenButton = styled(Button)`
 .MuiButton-label{
@@ -24,20 +44,23 @@ letter-spacing: -0.05em;
 `
 const MainWrapper = styled.div`
   background-color:black!important;
+  height: 100vh;
+  flex-grow: 1;
+  background-color: black;
+  color: white;
+padding :1%;
 
 `
 const about = (props) => {
   document.body.style = 'background:black;'
   return (
-    <MainWrapper className='main'>
+    <MainWrapper>
       <Grid container container direction="row" alignItems="center" justify="space-between" spacing={5} >
         <Grid item xs={12}>
           <Toolbar />
         </Grid>
         <Grid item xs={5}>
-          <div className="Bold" style={{
-            'white-space': 'pre-wrap'
-          }}>{"Agem helps your find you best \nLocal services such as\n"}
+          <Bold>{"Agem helps your find you best \nLocal services such as\n"}
             <TextLoop interval={350} >
               <span>Barbers </span>
               <span>Nail technicians</span>
@@ -45,18 +68,16 @@ const about = (props) => {
               <span>Gyms</span>
               <span>Fitness Coaches</span>
             </TextLoop>{" "} And Much more .
-            </div>
+            </Bold>
         </Grid>
         <Grid item xs={5} justify="flex-end" >
-          <img className="gym" src={gym} alt="gym" />
+          <Gym src={gym} alt="gym" />
         </Grid>
         <Grid item xs={6}>
-          <img className="doctor" src={doctor} alt="doctor" />
+          <Doc src={doctor} alt="doctor" />
         </Grid>
         <Grid item xs={6}>
-          <div className="Bold" style={{
-            'white-space': 'pre-wrap'
-          }}>{"Agem helps your find you best \nLocal services such as\n"}
+          <Bold>{"Agem helps your find you best \nLocal services such as\n"}
             <TextLoop interval={350} >
               <span>Barbers </span>
               <span>Nail technicians</span>
@@ -64,7 +85,7 @@ const about = (props) => {
               <span>Gyms</span>
               <span>Fitness Coaches</span>
             </TextLoop>{" "} And Much more .
-            </div>
+            </Bold>
         </Grid>
       </Grid>
     </MainWrapper >
@@ -72,8 +93,4 @@ const about = (props) => {
   )
 }
 
-
-{ }
-
-{/* */ }
 export default about; 
