cd591a6339a6f9921bb1a1a4b1eeba9713547a7f
Brodey Yendall <brodey_yendall@hotmail.com>
2020-08-22 18:36:37 +1000

    Hours: Removed old temp test db implementation

--- BackEnd/Hours/src/test/java/sept/major/hours/blackbox/PostBlackBoxTest.java
+++ BackEnd/Hours/src/test/java/sept/major/hours/blackbox/PostBlackBoxTest.java
@@ -56,26 +56,4 @@ public class PostBlackBoxTest extends HoursBlackBoxHelper {
         assertThat(getResult.getStatusCode()).isEqualTo(HttpStatus.ACCEPTED);
     }
 
-    protected String randomAlphanumericString(int length) {
-        final int[] uppercaseRange = {65, 91};
-        final int[] lowerCaseRange = {97, 123};
-        final int[] numbersRange = {48, 58};
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            int rangeToUse = (int) ((Math.random() * (4 - 1) + 1));
-            int charToUse;
-            if (rangeToUse == 1) {
-                charToUse = (int) ((Math.random() * (uppercaseRange[0] - uppercaseRange[1]) + uppercaseRange[1]));
-            } else if (rangeToUse == 2) {
-                charToUse = (int) ((Math.random() * (lowerCaseRange[0] - lowerCaseRange[1]) + lowerCaseRange[1]));
-            } else {
-                charToUse = (int) ((Math.random() * (numbersRange[0] - numbersRange[1]) + numbersRange[1]));
-            }
-            stringBuilder.append((char) charToUse);
-        }
-
-        return stringBuilder.toString();
-    }
-
 }
