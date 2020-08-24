11a6d6b2b654c6462154f20b26b5194ff0207a97
Brodey Yendall <brodey_yendall@hotmail.com>
2020-08-24 13:00:37 +1000

    Updated booking service schema

--- setup/docker/db/booking.sql
+++ setup/docker/db/booking.sql
@@ -1,8 +1,8 @@
-CREATE TABLE bookings
-(
-    booking_id serial primary key,
-    customer_id character varying  NOT NULL,
-    worker_id character varying  NOT NULL,
-    start_time timestamp NOT NULL,
-    end_time timestamp NOT NULL
+CREATE TABLE bookings (
+	booking_id serial NOT NULL,
+	customer_username varchar NOT NULL,
+	worker_username varchar NOT NULL,
+	start_date_time timestamp NOT NULL,
+	end_date_time timestamp NOT NULL,
+	CONSTRAINT bookings_pkey PRIMARY KEY (booking_id)
 );
