42492d880977a54fe63de787e50c85d74acde83b
s3707180 <s7@rmit.edu.au>
2020-09-06 12:05:50 +1000

    refactorng availability controller and adding "date" and "all" services

--- BackEnd/Availability/src/main/java/sept/major/availability/controller/AvailabilityController.java
+++ BackEnd/Availability/src/main/java/sept/major/availability/controller/AvailabilityController.java
@@ -1,5 +1,6 @@
 package sept.major.availability.controller;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -9,12 +10,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import sept.major.availability.entity.AvailabilityEntity;
 import sept.major.availability.entity.BookingEntity;
 import sept.major.availability.entity.HoursEntity;
 import sept.major.availability.service.AvailabilityService;
@@ -40,6 +44,10 @@ public class AvailabilityController {
 	private static final String HOURS_SERVICE_ENDPOINT = "hours.service.endpoint";
 	private static final String BOOKINGS_SERVICE_ENDPOINT = "bookings.service.endpoint";
 	
+	String userServiceEndpoint = env.getProperty(USER_SERVICE_ENDPOINT);
+	String hoursServiceEndpoint = env.getProperty(HOURS_SERVICE_ENDPOINT);
+	String bookingsServiceEndpoint = env.getProperty(BOOKINGS_SERVICE_ENDPOINT);
+
 	/**
 	 * This method will get user's available hours in the range get user's booked times overlay the booking on the availability calculate the result and return
 	 * as a list
@@ -50,17 +58,14 @@ public class AvailabilityController {
 	 * @param creatorUsername
 	 */
 	@GetMapping("/range")
-	public void getAvailabilityInRange(@RequestParam(name = "startDateTime") String startDateString,
+	public ResponseEntity getAvailabilityInRange(@RequestParam(name = "startDateTime") String startDateString,
 					@RequestParam(name = "endDateTime") String endDateString, @RequestParam(required = false) String workerUsername,
 					@RequestParam(required = false) String creatorUsername) {
 
 		log.info("startDateTime:{}, endDateTime:{}, workerUsername:{}, creatorUsername:{}, ", startDateString, endDateString, workerUsername, creatorUsername);
 		
-		String userServiceEndpoint = env.getProperty(USER_SERVICE_ENDPOINT);
-		String hoursServiceEndpoint = env.getProperty(HOURS_SERVICE_ENDPOINT);
-		String bookingsServiceEndpoint = env.getProperty(BOOKINGS_SERVICE_ENDPOINT);
-		
-		{//conversion test
+		try {
+		{//conversion test TODO remove
 			LocalDateTime sample = LocalDateTime.now();
 			log.info("sample:{}", sample);
 			LocalDateTime start_LDT = LocalDateTime.parse(startDateString);
@@ -68,25 +73,102 @@ public class AvailabilityController {
 			log.info("start_LDT:{}, end_LDT:{}", start_LDT, end_LDT);
 		}
 		
-		Map<String, String> hoursVariablesMap = new HashMap<String, String>();
-		hoursVariablesMap.put("startDateTime", startDateString);
-		hoursVariablesMap.put("endDateTime", endDateString);
-		hoursVariablesMap.put("workerUsename", workerUsername);
-		hoursVariablesMap.put("creatorUsername", creatorUsername);
+		Map<String, String> variablesMap = new HashMap<String, String>();
+		variablesMap.put("startDateTime", startDateString);
+		variablesMap.put("endDateTime", endDateString);
+		variablesMap.put("workerUsename", workerUsername);
+		variablesMap.put("creatorUsername", creatorUsername);
 		
 		String hoursTemplateUrl = hoursServiceEndpoint + "/range?startDateTime={startDateTime}&endDateTime={endDateTime}&workerUsename={workerUsename}&creatorUsername={creatorUsername}";
-		List<HoursEntity> hoursList = restTemplate.getForObject(hoursTemplateUrl, List.class, hoursVariablesMap);
+		List<HoursEntity> hoursList = restTemplate.getForObject(hoursTemplateUrl, List.class, variablesMap);
 
-		Map<String, String> bookingsVariablesMap = new HashMap<String, String>();
-		bookingsVariablesMap.put("startDateTime", startDateString);
-		bookingsVariablesMap.put("endDateTime", endDateString);
-		bookingsVariablesMap.put("workerUsername", workerUsername);
+		
 		
 		String bookingsTemplateUrl = bookingsServiceEndpoint + "/range?startDateTime={startDateTime}&endDateTime={endDateTime}&workerUsername={workerUsername}";
-		List<BookingEntity> bookingsList = restTemplate.getForObject(bookingsTemplateUrl, List.class, bookingsVariablesMap);
+		List<BookingEntity> bookingsList = restTemplate.getForObject(bookingsTemplateUrl, List.class, variablesMap);
 
 		log.info(hoursList.toString());  
 		
-		availabilityService.checkAllAvailabilities(hoursList, bookingsList);
+		List<AvailabilityEntity>  allAvailabilities = availabilityService.checkAllAvailabilities(hoursList, bookingsList);
+		
+		
+		return new ResponseEntity(allAvailabilities, HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST); //TODO try/catch to be improved
+		}
 	 }
+	
+	/**
+	 * Return availabilities for a given worker for a given date
+	 * @param dateString
+	 * @param workerUsername
+	 * @param creatorUsername
+	 * @return
+	 */
+    @GetMapping("/date")
+    public ResponseEntity getAvailabilityInDate(@RequestParam(name = "date") String dateString,
+                                         @RequestParam(required = false) String workerUsername,
+                                         @RequestParam(required = false) String creatorUsername) {
+    	
+    	log.info("dateString:{}, workerUsername:{}, creatorUsername:{}, ", dateString, workerUsername, creatorUsername);
+    	
+		try { // conversion test TODO remove
+			{// conversion test TODO remove
+				LocalDateTime sample = LocalDateTime.now();
+				log.info("sample:{}", sample);
+				LocalDateTime start_LD = LocalDateTime.parse(dateString);
+				log.info("start_LD:{}", start_LD);
+			}
+
+			Map<String, String> variablesMap = new HashMap<String, String>();
+			variablesMap.put("dateString", dateString);
+			variablesMap.put("workerUsename", workerUsername);
+			variablesMap.put("creatorUsername", creatorUsername);
+
+			String hoursTemplateUrl = hoursServiceEndpoint + "/date?date={dateString}&workerUsename={workerUsename}&creatorUsername={creatorUsername}";
+			List<HoursEntity> hoursList = restTemplate.getForObject(hoursTemplateUrl, List.class, variablesMap);
+
+			String bookingsTemplateUrl = bookingsServiceEndpoint + "/date?date={startDateTime}&workerUsername={workerUsername}";
+			List<BookingEntity> bookingsList = restTemplate.getForObject(bookingsTemplateUrl, List.class, variablesMap);
+
+			List<AvailabilityEntity> allAvailabilities = availabilityService.checkAllAvailabilities(hoursList, bookingsList);
+
+			return new ResponseEntity(allAvailabilities, HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST); // TODO try/catch to be improved
+		}
+
+    }
+
+    
+    /**
+     * return all available hours for the given worker and creator
+     * @param workerUsername
+     * @param creatorUsername
+     * @return
+     */
+    @GetMapping("/all")
+    public ResponseEntity getAllAvailabilities(@RequestParam(required = false) String workerUsername,
+                                      		@RequestParam(required = false) String creatorUsername) {
+    	log.info("workerUsername:{}, creatorUsername:{}, ", workerUsername, creatorUsername);
+
+		try {
+			Map<String, String> variablesMap = new HashMap<String, String>();
+			variablesMap.put("workerUsename", workerUsername);
+			variablesMap.put("creatorUsername", creatorUsername);
+
+			String hoursTemplateUrl = hoursServiceEndpoint + "/all?workerUsename={workerUsename}&creatorUsername={creatorUsername}";
+			List<HoursEntity> hoursList = restTemplate.getForObject(hoursTemplateUrl, List.class, variablesMap);
+
+			String bookingsTemplateUrl = bookingsServiceEndpoint + "/all?workerUsername={workerUsername}";
+			List<BookingEntity> bookingsList = restTemplate.getForObject(bookingsTemplateUrl, List.class, variablesMap);
+
+			List<AvailabilityEntity> allAvailabilities = availabilityService.checkAllAvailabilities(hoursList, bookingsList);
+
+			return new ResponseEntity(allAvailabilities, HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST); // TODO try/catch to be improved
+		}
+    }
 }
+
