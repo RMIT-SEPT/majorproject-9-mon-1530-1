0a6c476399fcf66adeb6da77a98475c6c39d3937
Lawrence A <30714521+LalaAU@users.noreply.github.com>
2020-09-07 22:45:09 +1000

    added in specific booking tests

--- BackEnd/Bookings/src/test/java/sept/major/bookings/unit/GetBulkEndpointTests.java
+++ BackEnd/Bookings/src/test/java/sept/major/bookings/unit/GetBulkEndpointTests.java
@@ -6,11 +6,13 @@ import org.springframework.http.ResponseEntity;
 import sept.major.bookings.entity.BookingEntity;
 import sept.major.common.response.ValidationError;
 
+import java.awt.print.Book;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -330,7 +332,7 @@ public class GetBulkEndpointTests extends UnitTestHelper {
         String startTime = "Today";
         String endTime = "Tomorrow";
 
-        when(mockedBookingRepository.findAllByStartDateTimeBetween(any(LocalDateTime.class), any(LocalDateTime.class))).thenThrow(new IllegalArgumentException());
+        //when(mockedBookingRepository.findAllByStartDateTimeBetween(any(LocalDateTime.class), any(LocalDateTime.class))).thenThrow(new IllegalArgumentException());
 
         ResponseEntity result = bookingServiceController.getRange(startTime, endTime, null, null);
 
@@ -344,7 +346,7 @@ public class GetBulkEndpointTests extends UnitTestHelper {
         String startTime = "Today";
         String endTime = "Tomorrow";
 
-        when(mockedBookingRepository.findAllByStartDateTimeBetween(any(LocalDateTime.class), any(LocalDateTime.class))).thenThrow(new IllegalArgumentException());
+        //when(mockedBookingRepository.findAllByStartDateTimeBetween(any(LocalDateTime.class), any(LocalDateTime.class))).thenThrow(new IllegalArgumentException());
 
         ResponseEntity result = bookingServiceController.getRange(LocalDateTime.now().toString(), endTime, null, null);
 
@@ -362,5 +364,44 @@ public class GetBulkEndpointTests extends UnitTestHelper {
         assertThat(result).isNotNull();
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(result.getBody()).isEqualTo("No start time was defined");
+    }
+
+    @Test
+    void getSpecificBooking() {
+        String workerUsername = randomAlphanumericString(20);
+        String customerUsername = randomAlphanumericString(20);
+        LocalDateTime startTime = LocalDateTime.now();
+        LocalDateTime endTime = startTime.toLocalDate().atStartOfDay().plusDays(1).minusSeconds(1);
+
+        BookingEntity entity = new BookingEntity(workerUsername, customerUsername, startTime, endTime);
+        entity.setBookingId(1);
+        when(mockedBookingRepository.findById(entity.getBookingId())).thenReturn(Optional.of(entity));
+
+        ResponseEntity result = bookingServiceController.getBooking(entity.getBookingId().toString());
+
+        assertThat(result).isNotNull();
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody()).isEqualTo(entity);
+    }
+
+    @Test
+    void getSpecificBookingNotExist() {
+        when(mockedBookingRepository.findById(1)).thenReturn(Optional.empty());
+
+        ResponseEntity result = bookingServiceController.getBooking("1");
+
+        assertThat(result).isNotNull();
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(result.getBody()).isEqualTo(String.format("No booking entity for id %d",1));
+    }
+
+    @Test
+    void getSpecificBookingBadDate() {
+        String id = LocalDateTime.now().toString();
+        ResponseEntity result = bookingServiceController.getBooking(id);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(result.getBody()).isEqualTo(String.format("For input string: \"%s\"",id));
     }
 }
