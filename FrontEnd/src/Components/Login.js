b11d49b31bf883ac575d5af7e496b36261edf32f
rw22448 <rw22448@gmail.com>
2020-09-07 22:07:29 +1000

    Remove unused variables

--- FrontEnd/src/Components/Login.js
+++ FrontEnd/src/Components/Login.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import { Button, Grid } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import logo from '../media/logo.png';
