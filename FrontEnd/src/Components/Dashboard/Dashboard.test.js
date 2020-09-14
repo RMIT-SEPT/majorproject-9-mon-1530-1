31a66acec7b1f035783c1ccf8dbc86a2ca9cb73c
rw22448 <rw22448@gmail.com>
2020-09-14 12:19:56 +1000

    Write tests for full coverage on Dashboard

---
+++ FrontEnd/src/Components/Dashboard/Dashboard.test.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { DashboardWrapper, MenuBarComponent } from './Dashboard';
+import { theme } from '../../App';
+
+// Added this render method to wrap components rendered in Theme tag to prevent
+// test failing
+
+const renderComponent = (component) => {
+  return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
+};
+
+describe('Dashboard', () => {
+  describe('DashboardWrapper', () => {
+    const bookingMock = jest.fn();
+
+    const userName = 'testId123';
+    const role = 'User';
+    const actions = { bookingLink: bookingMock };
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('should render a new Dashboard successfully when passing in required props', () => {
+      const { getByText } = renderComponent(
+        <DashboardWrapper
+          userName={userName}
+          role={role}
+          actions={actions}
+        ></DashboardWrapper>
+      );
+
+      expect(getByText(userName)).toBeTruthy();
+      expect(getByText(role)).toBeTruthy();
+    });
+
+    it('should render children components inside Dashboard when provided', () => {
+      const { getByText } = renderComponent(
+        <DashboardWrapper userName={userName} role={role} actions={actions}>
+          <div>Hello World!</div>
+        </DashboardWrapper>
+      );
+
+      expect(getByText('Hello World!')).toBeTruthy();
+    });
+
+    it('should display placeholder info when props not provided', () => {
+      // TODO: Handle or enforce passing of actions for navigation links
+
+      const { getAllByText } = renderComponent(
+        <DashboardWrapper actions={actions}></DashboardWrapper>
+      );
+
+      expect(getAllByText('empty')).toHaveLength(2);
+    });
+
+    describe('actions', () => {
+      it('should fire a callback when clicking on New Booking', () => {
+        const { getByText } = renderComponent(
+          <DashboardWrapper
+            userName={userName}
+            role={role}
+            actions={actions}
+          ></DashboardWrapper>
+        );
+
+        fireEvent.click(getByText('New Booking'));
+
+        expect(bookingMock).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('MenuBarComponent', () => {
+    it('should render the MenuBarComponent with default LogOut icon', () => {
+      const { getByTestId } = renderComponent(
+        <MenuBarComponent></MenuBarComponent>
+      );
+
+      expect(getByTestId('logOutIcon')).toBeTruthy();
+    });
+
+    it('should render all children components passed under MenuBarComponent', () => {
+      const { getAllByRole } = renderComponent(
+        <MenuBarComponent>
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+        </MenuBarComponent>
+      );
+
+      expect(getAllByRole('button')).toHaveLength(3);
+    });
+  });
+});
