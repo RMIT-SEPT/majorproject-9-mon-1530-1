35d2dd9f659d42494a8811d217c18566d2742104
Brodey Yendall <brodey_yendall@hotmail.com>
2020-10-06 13:25:27 +1100

    Users: Update tests with the latest security implementation

--- BackEnd/Users/src/main/resources/user.sql
+++ BackEnd/Users/src/main/resources/user.sql
@@ -1,9 +1,19 @@
 CREATE TABLE users
 (
-  username   varchar NOT NULL,
-  "password" varchar NOT NULL,
-  userType   varchar NULL,
-  "name"     varchar NULL,
-  phone      varchar NULL,
-  address    varchar NULL
+  username   varchar      NOT NULL,
+  "password" varchar      NOT NULL,
+  userType   varchar      NULL,
+  "name"     varchar      NULL,
+  phone      varchar      NULL,
+  address    varchar      NULL,
+  "token"    varchar(255) null
 );
+
+INSERT INTO public.users
+  (username, "password", usertype, "name", phone, address, "token")
+VALUES ('ValidUser', 'test password', 'User', 'Test name', 'Test phone', 'Test address', 'ValidUserToken');
+
+INSERT INTO public.users
+  (username, "password", usertype, "name", phone, address, "token")
+VALUES ('ValidAdmin', 'test password', 'Admin', 'Test name', 'Test phone', 'Test address', 'ValidAdminToken');
+
---
+++ BackEnd/Users/src/test/java/sept/major/users/blackbox/security/AccessBlackBoxTest.java
@@ -0,0 +1,131 @@
+package sept.major.users.blackbox.security;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import sept.major.users.blackbox.UserBlackBoxHelper;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AccessBlackBoxTest extends UserBlackBoxHelper {
+
+    @Test
+    @DisplayName("Delete request with various token")
+    public void deleteAuthenticationTests() {
+        // Attempt delete without valid token
+        ResponseEntity response = deleteRequest(getUrl(), getUnAuthorizedHeaders());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        // Attempt delete with user token
+        response = deleteRequest(getUrl(), getAuthorizedUserHeaders());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+
+        // Attempt delete with admin token
+        response = deleteRequest(getUrl(), getAuthorizedAdminHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    @Test
+    @DisplayName("Patch request with various token")
+    public void patchAuthenticationTests() {
+        // Attempt patch without valid token
+        ResponseEntity response = patchRequest(getUrl(), getUnAuthorizedHeaders(new HashMap<>()));
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        // Attempt patch with user token
+        response = patchRequest(getUrl(), getAuthorizedUserHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt patch with admin token
+        response = patchRequest(getUrl(), getAuthorizedAdminHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    @Test
+    @DisplayName("Patch password request with various token")
+    public void patchPasswordAuthenticationTests() {
+        // Attempt patch without valid token
+        ResponseEntity response = patchRequest(getUrl("password"), getUnAuthorizedHeaders(new HashMap<>()));
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        // Attempt patch with user token
+        response = patchRequest(getUrl("password"), getAuthorizedUserHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt patch with admin token
+        response = patchRequest(getUrl("password"), getAuthorizedAdminHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    @Test
+    @DisplayName("Post request with various token")
+    public void postAuthenticationTests() {
+        // Attempt post without valid token
+        ResponseEntity response = postRequest(getUnAuthorizedHeaders(new HashMap<>()));
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt post with user token
+        response = postRequest(getAuthorizedUserHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt post with admin token
+        response = postRequest(getAuthorizedAdminHeaders());
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    @Test
+    @DisplayName("Get bulk request with various token")
+    public void getBulkAuthenticationTests() {
+        // Attempt get all without valid token
+        ResponseEntity response = getRequest(getUrl("bulk"), getUnAuthorizedHeaders(), String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        // Attempt get all with user token
+        response = getRequest(getUrl("bulk"), getAuthorizedUserHeaders(), String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+
+        // Attempt get all with admin token
+        response = getRequest(getUrl("bulk"), getAuthorizedAdminHeaders(), String.class);
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    @Test
+    @DisplayName("Get by username with various token")
+    public void getByUsernameAuthenticationTests() {
+        // Attempt get by id without valid token
+        ResponseEntity response = getRequest(getUrl("username"), getUnAuthorizedHeaders(), String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+
+        // Attempt get by id with user token
+        response = getRequest(getUrl("username"), getAuthorizedUserHeaders(), String.class);
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt get by id with admin token
+        response = getRequest(getUrl("username"), getAuthorizedAdminHeaders(), String.class);
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    @Test
+    @DisplayName("Get by username with various token")
+    public void getTokenAuthenticationTests() {
+        // Attempt get by id without valid token
+        ResponseEntity response = getRequest(getUrl("token"), getUnAuthorizedHeaders(), String.class);
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt get by id with user token
+        response = getRequest(getUrl("token"), getAuthorizedUserHeaders(), String.class);
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+
+        // Attempt get by id with admin token
+        response = getRequest(getUrl("token"), getAuthorizedAdminHeaders(), String.class);
+        assertTrue(requestAuthenticated(response.getStatusCode()));
+    }
+
+    private boolean requestAuthenticated(HttpStatus httpStatus) {
+        return !HttpStatus.UNAUTHORIZED.equals(httpStatus) && !HttpStatus.FORBIDDEN.equals(httpStatus);
+    }
+}
